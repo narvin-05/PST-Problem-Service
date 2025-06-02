65149a03bcc78f60f6783e656b9322bb9673bb4b
Narvin Patidar <narvin.patidar@classplus.co>
2025-06-03 01:03:30 +0530

    added basic express app with dotenv & nodemon

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const {PORT} = require('./config/server.config');
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.text());
+
+
+app.get('/ping', (req,res) =>{
+    res.send({"message":"Problem service is alive"})
+})
+
+app.listen(PORT, (req,res) =>{
+    console.log(`Server is up and running at ${PORT}`);
+})
