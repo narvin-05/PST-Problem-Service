269b481022c0b0993699e183fe5ac82da5af6920
Narvin Patidar <narvin.patidar@classplus.co>
2025-06-03 15:04:05 +0530

    Fix: added status codes instead of numeric values

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,33 +1,35 @@
+const {STATUS_CODES} = require('http-status-codes');
+
 function pingProblemController(req,res,next){
     res.json({message:"Problem controller is up and working"});
 }
 
 function addProblem(req,res,next){
-    return res.status(501).json({
+    return res.status(STATUS_CODES.NOT_IMPLEMENTED).json({
         message:"Not implemented yet"
     })
 }
 
 function getProblem(req,res,next){
-    return res.status(501).json({
+    return res.status(STATUS_CODES.NOT_IMPLEMENTED).json({
         message:"Not implemented yet"
     })
 } 
 
 function getProblems(req,res,next){
-    return res.status(501).json({
+    return res.status(STATUS_CODES.NOT_IMPLEMENTED).json({
         message:"Not implemented yet"
     })
 }
 
 function deleteProblem(req,res,next){
-    return res.status(501).json({
+    return res.status(STATUS_CODES.NOT_IMPLEMENTED).json({
         message:"Not implemented yet"
     })
 }
 
 function updateProblem(req,res,next){
-    return res.status(501).json({
+    return res.status(STATUS_CODES.NOT_IMPLEMENTED).json({
         message:"Not implemented yet"
     })
 }
